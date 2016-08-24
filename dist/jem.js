d9e2b48856f1023bed6023c992dc70f7749a4a4f
DHkamp <dhoelkeskamp@live.de>
2016-08-24 12:45:00 +0200

    New dist

--- dist/jem.js
+++ dist/jem.js
@@ -56,6 +56,11 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	'use strict';
 	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	exports.JEM = undefined;
+	
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 	
 	var _eventManager = __webpack_require__(1);
@@ -85,8 +90,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  return Jem;
 	}(_eventManager.EventManager);
 	
-	window.JEM = new Jem();
-	// export default new Jem();
+	var JEM = exports.JEM = new Jem();
 
 /***/ },
 /* 1 */
